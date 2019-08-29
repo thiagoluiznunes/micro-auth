c0f2730519dc9962a889a90289da43664f524313
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-08-29 19:03:23 -0300

    create change behavior in form

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,12 @@
-console.log('It is workings!');
+const createAccount = document.getElementById('createAccount');
+const loginAccount = document.getElementById('loginAccount');
+
+loginAccount.addEventListener('click', () => {
+  document.getElementById('signupForm').style.display = 'none';
+  document.getElementById('loginForm').style.display = 'block';
+});
+
+createAccount.addEventListener('click', () => {
+  document.getElementById('loginForm').style.display = 'none';
+  document.getElementById('signupForm').style.display = 'block';
+});
